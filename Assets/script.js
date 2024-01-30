9b82ea3fad91b28cfd167728c71d43fe5998992d
Fernanda <152433962+MaFer74@users.noreply.github.com>
2024-01-30 00:28:03 +0000

    formating of html, added some jS code

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,38 @@
-const searchBtn = document.getElementById('search-button')
-const apiKey = 'eb31a46b5d14ea5586c11a6c70dfa7e9'
-const todayContainer = document.getElementById('today')
-const idCity = document.getElementById("search-input")
-function start(event){
-    event.preventDefault()
-currentWeather()
+const searchBtn = document.getElementById("search-button");
+const apiKey = "eb31a46b5d14ea5586c11a6c70dfa7e9";
+const todayContainer = document.getElementById("today");
+const idCity = document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
+function start(event) {
+  event.preventDefault();
+  currentWeather(idCity.value);
 }
 
-function currentWeather(){
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiKey).then(function(response){
-    return response.json()
-}).then(function (data){
-    console.log(data);
-    const cityNameEl =  document.createElement('h2')
-    cityNameEl.textContent = data.name
-
-    todayContainer.append(cityNameEl)
-})
-
+function currentWeather(city) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey +
+      "&units=metric"
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      const cityNameEl = document.createElement("h2");
+      cityNameEl.textContent = data.name;
+      todayContainer.append(cityNameEl);
+      //document.getElementById('city-name').textContent=data.name
+     
+      // Transfer content to HTML
+    $(".city-name").html("<h2>" + data.name + " Weather Details</h2>");
+    $(".main-wind").text("Wind Speed: " + data.wind.speed);
+    $(".main-humidity").text("Humidity: " + data.main.humidity);
+    });
 }
 
-searchBtn.addEventListener('submit', start)
+searchForm.addEventListener("submit", start);
+console.log("testing");
